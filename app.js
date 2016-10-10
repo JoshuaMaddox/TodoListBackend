fdcb553e7c795991e55cca2f9175c988d39c3ce0
Joshua Maddox <HomeFolder@Joshuas-MacBook-Pro.local>
2016-10-10 10:42:10 -0700

    add routes

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const PORT = 8000,
       webpack = require('webpack'),
       bodyParser = require('body-parser'),
       webpackConfig = require('./webpack.config'),
-      dataHandlers = require('./models/dataHandlers.js'),
+      todoHandlers = require('./models/todoHandlers.js'),
       webpackDevMiddleware = require('webpack-dev-middleware'),
       webpackHotMiddleware = require('webpack-hot-middleware')
 
@@ -27,9 +27,61 @@ app.use(webpackDevMiddleware(compiler, {
 }))
 app.use(webpackHotMiddleware(compiler))
 
+//Get a list of all Todos by completion status
+// app.get('/todos', (req, res) => {
+//   todoHandlers.getAllToDos((err, allToDos) => {
+//     if(err) {
+//       return res.status(400).send(err)
+//     }
+//     res.send(allToDos)
+//   })
+// })
 
-app.get('/test', (req, res) => {
-  res.send('Hiya buddy')
+
+//Get a list of all Todos
+app.get('/todos', (req, res) => {
+  let queries = req.query
+  console.log('I am the query', queries)
+  todoHandlers.getAndFilterTodos(queries, (err, allToDos) => {
+    if(err) {
+      return res.status(400).send(err)
+    }
+    res.send(allToDos)
+  })
+})
+
+//Create a new todo
+app.post('/todos', (req, res) => {
+  todoHandlers.createToDo(req.body, err => {
+    if(err) return res.status(400).send(err);
+    res.send({success: true});
+  })
+})
+
+//Edit Todo's Status
+app.put('/todos/:id', (req, res) => {
+  let id = req.params.id
+  todoHandlers.editToDo(id, (err) => {
+    if(err) return res.status(404).send(err)
+  })
+  res.send({success: true})
+})
+
+//Delete all complete todos 
+app.delete('/todos/complete', (req, res) => {
+  todoHandlers.deleteCompleted((err, remainingTodos) => {
+    if(err) return res.status(404).send(err)
+  res.send(remainingTodos)
+  })
+})
+
+//Delete a todo 
+app.delete('/todos/:id', (req, res) => {
+  let deleteID = req.params.id
+  todoHandlers.deleteToDo(deleteID, (err) => {
+    if(err) return res.status(404).send(err)
+  })
+  res.send(`ToDo ${deleteID}, was deleted from your deck`)
 })
 
 app.listen(PORT, err => {
---
+++ models/todoHandlers.js
@@ -0,0 +1,129 @@
+const fs = require('fs'),
+      path = require('path'),
+      uuid = require('uuid')
+      todos = path.join(__dirname, '../data/todo.json')
+
+//Get all ToDos in a Readable Format
+exports.getAllToDos = function(cb){
+  fs.readFile(todos, (err, buffer) => {
+    if(err) return cb(err)
+      try{
+        var allToDos = JSON.parse(buffer)
+      } catch(e) {
+        var allToDos = []
+      }
+      cb(null, allToDos)
+  })
+}
+
+
+//Write whatever is fed in to JSON
+exports.write = function(newData, cb) {
+  let json = JSON.stringify(newData)
+  fs.writeFile(todos, json, cb)
+}
+
+//Get all Todos and filter results
+// exports.getAndFilterTodos = function(completionStatus) {
+//   exports.getAllToDos((err, allToDos) => {
+//     let filterdToDos = allToDos.filter((todo) => {
+//       if(completionStatus === todo.status){
+//         return todo
+//       } else {
+//         return
+//       }
+//     })
+//     cb(null, filterdToDos)
+//   })
+// }
+
+//Get All todos and also filter results if requested
+exports.getAndFilterTodos = function(completionStatus, cb) {
+  let filterdToDos;
+  console.log('I am completionStatus: ', completionStatus)
+  if(completionStatus.complete){
+    console.log("I've been skipped")
+    exports.getAllToDos((err, allToDos) => {
+      filterdToDos = allToDos.filter((todo) => {
+        if(completionStatus.complete == todo.status.toString()){
+          return todo
+        } else {
+          return
+        }
+      })
+    cb(null, filterdToDos) 
+    })
+  }
+  else {
+    exports.getAllToDos((err, allToDos) => {
+      console.log('I am allToDos', allToDos)
+      filterdToDos = allToDos
+    cb(null, filterdToDos)
+    })
+  }
+}
+
+//Creat a new ToDo
+exports.createToDo = function(newToDo, cb) {
+  exports.getAllToDos((err, todo) => {
+    if(err) return cb(err)
+    newToDo.id = uuid()
+    newToDo.status = false
+    console.log('I am the newToDo', newToDo) 
+    todo.push(newToDo)
+    exports.write(todo, cb)
+  })
+}
+
+//Update a ToDo's Status
+exports.editToDo = function(id, cb) {
+  exports.getAllToDos((err, ToDos) => {
+    if(err) return cb(err)
+    let editedToDos = ToDos.map((todo, index) => {
+      if(id === todo.id) {
+        todo.status = !todo.status
+        return todo
+      } else {
+        return todo
+      }
+    })
+    exports.write(editedToDos)
+  })
+}
+
+//Delete toDo by ID
+exports.deleteToDo = function(deleteID, cb) {
+  exports.getAllToDos((err, todos) => {
+    if(err) return cb(err)
+    let newToDos = todos.filter((todo, index) => {
+      if(deleteID !== todo.id){
+        return todo
+      } 
+    })
+    exports.write(newToDos)  
+  })
+  cb()
+}
+
+//Delete all Completed Todos
+exports.deleteCompleted = function(cb) {
+  exports.getAllToDos((err, todos) => {
+    if(err) return cb(err)
+    let remainingToDos = todos.filter((todo) => {
+      if(!todo.status){
+        return todo
+      } 
+    })
+    exports.write(remainingToDos)  
+  cb(remainingToDos)
+  })
+}
+
+
+
+
+
+///DUMPING GROUND-------------------
+
+
+
